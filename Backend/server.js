44a07e5442d3f592b1e55fc9ebfb5a44a6890c77
Ashish <ashishsingh272007@gmail.com>
2025-03-10 18:05:03 +0530

    Deployment-Setup and Deploy

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,5 @@
 // USING ES6(MODULE) NOT ES5(COMMONJS)
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './Routes/auth.routes.js';
@@ -9,6 +10,7 @@ import cookieParser from 'cookie-parser';
 import { app, server } from './socket/socket.js';
 dotenv.config();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body);
 app.use(cookieParser()); // to parse the incoming cookies from req.cookies;
@@ -16,11 +18,23 @@ app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/kabutar/dist")));
+
 app.get('*', (req, res) => {
-    res.send("Route doesn't exists");
+    res.sendFile(path.join(__dirname, "frontend/kabutar", "dist", "index.html"));
+    // both our server and frontend are running on the same port, that's exactly what is going to happen in our deployed server
 })
 
 server.listen(PORT, () => {
     connectToDB()
     console.log(`Server is listening at port ${PORT}`);
 })
+
+
+
+
+// in root package.json 
+// for not nested files --> 
+// "build": "npm install && npm install --prefix frontend/kabutar && npm run build --prefix frontend/kabutar"
+// for nested files --> 
+// "build": "npm install && npm install --prefix frontend/kabutar && npm run build --prefix frontend/kabutar"
